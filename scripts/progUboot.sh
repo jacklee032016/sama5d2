bade98037d96d611fa9a90034c7beb8eba083e35
jack <jacklee032016@gmail.com>
2019-08-23 17:09:38 -0400

    add binary preparing for release

--- scripts/progUboot.sh
+++ scripts/progUboot.sh
@@ -4,7 +4,7 @@ DIR_SRC=`dirname "$0"`
 
 source $DIR_SRC/hwConfig.sh
 
-UBOOT_BIN=u-boot.bin
+UBOOT_BIN=Binaries/xpld/Sama5d2Xpld.u-boot.bin
 
 CMD="sam-ba -p $HW_PORT $ATMEL_DEVICE -a $APPLET_QSPI -c erase:$UBOOT_START:$UBOOT_SIZE -c write:$UBOOT_BIN:$UBOOT_START"
 
